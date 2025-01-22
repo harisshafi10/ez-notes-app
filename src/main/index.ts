f62f02c0f2df6677c2fea8696272b9f73ac729b0
haris shafi <146629838+harisshafi10@users.noreply.github.com>
2025-01-22 13:48:58 +0530

    Update index.ts

--- src/main/index.ts
+++ src/main/index.ts
@@ -30,8 +30,8 @@ app.whenReady().then(() => {
 })
 
 // Quit when all windows are closed, except on macOS
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit()
+app.on('window-all-closed', () => { 
+  if (process.platform !== 'darwin') { 
+    app.quit() 
   }
 })
